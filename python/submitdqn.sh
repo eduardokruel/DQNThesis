65f3ec533fe20928335ed367dfeb5a97e7929538
Eduardo Kruel <eduardo@kruel.xyz>
2024-04-29 12:25:01 +0200

    Update submitdqn.sh

--- python/submitdqn.sh
+++ python/submitdqn.sh
@@ -7,7 +7,7 @@ cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR/etl360
 cd $TMPDIR/etl360/python/
 conda init
 conda activate dqn39
-python dqn.py
+python dqn.py --seed 1 --env-id CartPole-v1 --total-timesteps 500000 --track
 mkdir -p $HOME/slurm-bpai-tutorial/python/results
 cp -a /runs/. $HOME/slurm-bpai-tutorial/python/results
 rm -rf $TMPDIR/etl360/python
