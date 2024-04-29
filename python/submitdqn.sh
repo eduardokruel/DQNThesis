2af0eb87587fe6b6b096ebd5c373e99258220052
Eduardo Kruel <eduardo@kruel.xyz>
2024-04-29 12:21:01 +0200

    Update submitdqn.sh

--- python/submitdqn.sh
+++ python/submitdqn.sh
@@ -2,9 +2,12 @@
 #SBATCH --time=00:15:00
 #SBATCH -N 1
 
-
+mkdir -p $TMPDIR/etl360
 cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR/etl360
 cd $TMPDIR/etl360/python/
+conda init
+conda activate dqn39
 python dqn.py
 mkdir -p $HOME/slurm-bpai-tutorial/python/results
 cp -a /runs/. $HOME/slurm-bpai-tutorial/python/results
+rm -rf $TMPDIR/etl360/python
