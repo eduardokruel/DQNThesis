e216fe58338058733640f5e351037e0e084a1867
Eduardo Kruel <eduardo@kruel.xyz>
2024-05-03 21:58:41 +0200

    Update submitdqnparallel.sh

--- python/submitdqnparallel.sh
+++ python/submitdqnparallel.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #SBATCH --time=00:15:00
-#SBATCH -N 1
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=2
 
 mkdir -p $TMPDIR/etl360
 cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR/etl360
@@ -10,5 +11,5 @@ conda activate dqn39
 python dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id CartPole-v1 --total-timesteps 500000 --track --wandb-project-name cleanrldas --capture_video
 ls
 mkdir -p $HOME/slurm-bpai-tutorial/python/results
-cp -a /runs/. $HOME/slurm-bpai-tutorial/python/results
+cp -r $TMPDIR/etl360/python/wandb $HOME/slurm-bpai-tutorial/python/results
 rm -rf $TMPDIR/etl360/python
