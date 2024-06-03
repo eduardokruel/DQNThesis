c742e33b899e70eb6df9ad906f344294d0b0a1ae
Eduardo Kruel <eduardo@kruel.xyz>
2024-06-03 20:13:03 +0200

    evaluation

--- python/submitevaluate.sh
+++ python/submitevaluate.sh
@@ -7,7 +7,7 @@ cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR/etl360
 cd $TMPDIR/etl360/python/
 conda init
 conda activate dqn39
-python evaluate.py --seed 1 --kill_reward 0 --save_model "wizard_of_wor_v3__experiment__1__1717437083"
+python evaluate.py --seed 1 --kill_reward 0 --save_path wizard_of_wor_v3__experiment__1__1717437083
 mkdir -p $HOME/slurm-bpai-tutorial/python/results
 cp -r $TMPDIR/etl360/python/runs/ $HOME/slurm-bpai-tutorial/python/results
 rm -rf $TMPDIR/etl360/python
